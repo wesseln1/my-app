716325528900a69efdea6d8f71694e38b7dc9421
wesseln1 <nwessel429@gmail.com>
2018-11-16 13:58:50 -0600

    locations posting into dom

---
+++ src/components/Locations/locationsList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+export default class LocationList extends Component {
+    render(){
+        return(
+            <article>
+                <h1>Locations</h1>
+                <section>Nashville North: 1505 Demonbreun St, Nashville TN, 37203</section>
+                <section>Nashville South: 1414 Poplar ave, Nashville TN, 37208</section>
+            </article>
+        )
+    }
+}
